b5c0a52c06d1d32f9cf1eaeb98c17bcb74177f68
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-08 10:02:24 -0500

    Updated the view

--- src/main/java/com/example/w23comp1008lhw5memorygame/MemoryGameController.java
+++ src/main/java/com/example/w23comp1008lhw5memorygame/MemoryGameController.java
@@ -1,4 +1,24 @@
 package com.example.w23comp1008lhw5memorygame;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+
 public class MemoryGameController {
+
+    @FXML
+    private Label correctGuessLabel;
+
+    @FXML
+    private FlowPane flowPane;
+
+    @FXML
+    private Label guessesLabel;
+
+    @FXML
+    void playAgain(ActionEvent event) {
+
+    }
+
 }
