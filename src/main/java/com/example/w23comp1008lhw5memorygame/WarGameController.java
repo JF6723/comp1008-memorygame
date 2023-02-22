6c7d16a195daa5c6788109e21bebd496e11e2de5
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-22 10:21:02 -0500

    Week 7 - Completed the logic for the war game

--- src/main/java/com/example/w23comp1008lhw5memorygame/WarGameController.java
+++ src/main/java/com/example/w23comp1008lhw5memorygame/WarGameController.java
@@ -38,6 +38,7 @@ public class WarGameController implements Initializable {
     void playHand() {
         player1ImageView.setVisible(true);
         player2ImageView.setVisible(true);
+        cardsInPileLabel.setVisible(true);
 
         if (player1Hand.size()==0)
             declareWinner("player 2");
@@ -53,6 +54,43 @@ public class WarGameController implements Initializable {
             Card p2Card = player2Hand.remove(0);
             player2ImageView.setImage(p2Card.getImage());
             centerPileOfCards.add(p2Card);
+            updateLabels();
+
+            //check who wins the hand
+            if (p1Card.getCardValue() == p2Card.getCardValue())
+            {
+                warHand();
+            }
+
+            else if (p1Card.getCardValue() > p2Card.getCardValue())
+            {
+                player1Hand.addAll(centerPileOfCards);
+                centerPileOfCards.clear();
+            }
+            else {
+                player2Hand.addAll(centerPileOfCards);
+                centerPileOfCards.clear();
+            }
+        }
+    }
+
+    /**
+     * If the 2 cards played have the same value, we have a "war hand".  Each player removes 3 cards
+     * from their hand, then it proceeds like a regular hand (but with more cards at risk of being lost)
+     */
+    private void warHand()
+    {
+        if (player1Hand.size()<4)
+            declareWinner("player 2");
+        else if (player2Hand.size()<4)
+            declareWinner("player 1");
+        else{
+            centerPileOfCards.add(player1Hand.remove(0));
+            centerPileOfCards.add(player1Hand.remove(0));
+            centerPileOfCards.add(player1Hand.remove(0));
+            centerPileOfCards.add(player2Hand.remove(0));
+            centerPileOfCards.add(player2Hand.remove(0));
+            centerPileOfCards.add(player2Hand.remove(0));
         }
     }
 
@@ -82,5 +120,20 @@ public class WarGameController implements Initializable {
             player1Hand.add(deck.dealTopCard());
             player2Hand.add(deck.dealTopCard());
         }
+
+        //force a "war" hand to be first
+        Card ace = new Card("spades","ace");
+        player1Hand.add(0,ace);
+        player2Hand.add(0,ace);
+    }
+
+    /**
+     * This method will show how many cards are in each players' hand as well as the center pile
+     */
+    private void updateLabels()
+    {
+        player1CardCountLabel.setText(player1Hand.size() + " Cards");
+        player2CardCountLabel.setText(player2Hand.size() + " Cards");
+        cardsInPileLabel.setText(centerPileOfCards.size() + " Cards");
     }
 }
