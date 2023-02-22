355163197ece09453732685a57837e58232ea9f9
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-22 08:46:45 -0500

    Week 7 - updated the view layout for the War game

--- src/main/java/com/example/w23comp1008lhw5memorygame/WarGameController.java
+++ src/main/java/com/example/w23comp1008lhw5memorygame/WarGameController.java
@@ -1,4 +1,34 @@
 package com.example.w23comp1008lhw5memorygame;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class WarGameController {
+
+    @FXML
+    private Label cardsInPileLabel;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Label player1CardCountLabel;
+
+    @FXML
+    private ImageView player1ImageView;
+
+    @FXML
+    private Label player2CardCountLabel;
+
+    @FXML
+    private ImageView player2ImageView;
+
+    @FXML
+    void playHand(ActionEvent event) {
+
+    }
+
 }
